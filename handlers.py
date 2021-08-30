2c0532643b10e81998c75cd7822362a621d1a0f8
username <spiridonovfed@yandex.ru>
2021-08-30 21:09:03 +0300

    delete notification on bot's start command

--- handlers.py
+++ handlers.py
@@ -6,14 +6,6 @@ from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID
 def start(update, context):
     update.message.reply_text(WELCOME_MESSAGE)
 
-#     user_info = update.message.from_user.to_dict()
-#
-#     context.bot.send_message(
-#         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
-#         text=f"""
-# 📞 Connected {user_info}.
-#         """,
-#     )
 
 
 def forward_to_chat(update, context):
